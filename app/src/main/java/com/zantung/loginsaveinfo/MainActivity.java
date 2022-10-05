ec7874acf25179b4fd0f7472db0d277398794771
tungf20t3 <20t3.luuvantung@gmail.com>
2022-10-05 09:16:22 +0700

    chinh sua

--- app/src/main/java/com/zantung/loginsaveinfo/MainActivity.java
+++ app/src/main/java/com/zantung/loginsaveinfo/MainActivity.java
@@ -2,25 +2,48 @@ package com.zantung.loginsaveinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnLogin;
     EditText User, pass;
     CheckBox cbSavePass;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         anhxa();
+        sharedPreferences = getSharedPreferences("dataLogin", MODE_PRIVATE);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                String username = User.getText().toString().trim();
+                String password = pass.getText().toString().trim();
+                if(username.equals("vantung") && password.equals("123")){
+                    Toast.makeText(MainActivity.this, "đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                    if (cbSavePass.isChecked()){
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("taikhoan", username);
+                        editor.putString("matkhau", password);
+                        editor.putBoolean("checked", true);
+                        editor.commit();
+                    }else {
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.remove("taikhoan");
+                        editor.remove("matkhau");
+                        editor.remove("checked");
+                        editor.commit();
+                    }
+                }else {
+                    Toast.makeText(MainActivity.this, "Lỗi đăng nhập!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
